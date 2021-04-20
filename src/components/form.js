480162fa23624bab2fcb9a6ca990a155a07fbaef
Arsenii <arsenii.mingazov@belstu.by>
2021-04-20 18:28:41 +0300

    Implemented axios and created a login method

--- src/components/form.js
+++ src/components/form.js
@@ -1,7 +1,7 @@
 import React from 'react';
-import API from './baseURL';
+import API from '../baseURL';
 
-export default class App extends React.Component {
+export default class Form extends React.Component {
   handleChangeEmail = (event) => {
     this.setState({ email: event.target.value });
   }
