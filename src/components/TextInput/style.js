361460c741448d7107260d8542cbe420bb9206a0
Arsenii <arsenii.mingazov@belstu.by>
2021-04-27 17:39:16 +0300

    Changed stylelint rules, fixed pre-commit commands

--- src/components/TextInput/style.js
+++ src/components/TextInput/style.js
@@ -1,18 +1,16 @@
 import styled from 'styled-components';
 
-const TextField = styled.input`
-	font-size: 20px;
-	margin-bottom: 15px;
-	padding: 5px 10px;
-	border-radius: 5px;
-	border: 1px solid #dedede;
-	background-color: #f2f2f2;
+export const TextField = styled.input`
+  font-size: 20px;
+  margin-bottom: 15px;
+  padding: 5px 10px;
+  border-radius: 5px;
+  border: 1px solid #dedede;
+  background-color: #f2f2f2;
 `;
 
-const ErrorMessage = styled.p`
-	font-size: 18px;
-	color: red;
-	text-align: left;
+export const ErrorMessage = styled.p`
+  font-size: 18px;
+  color: red;
+  text-align: left;
 `;
-
-export { TextField, ErrorMessage };
